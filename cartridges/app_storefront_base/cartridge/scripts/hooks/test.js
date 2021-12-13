d75bffc989a86f64d639f1a3a7a7e368b6cee916
radinamanolova <radina.manolova96@gmail.com>
2021-12-13 19:21:10 +0200

    First commit

---
+++ cartridges/app_storefront_base/cartridge/scripts/hooks/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function testText() {
+
+}
+
+exports.testText = testText;
---
+++ cartridges/plugin_instorepickup/test/mocks/scripts/cart/baseCartHelpers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var BaseCartHelpers = {
+    addLineItem: function (currentBasket,
+                           product,
+                           quantity,
+                           childProducts,
+                           optionModel,
+                           defaultShipment) {
+        var productLineItem = currentBasket.createProductLineItem(product,
+            optionModel,
+            defaultShipment);
+        productLineItem.setQuantityValue(quantity);
+        return productLineItem;
+    },
+    getQtyAlreadyInCart: function () { return 1; },
+    getExistingProductLineItemsInCart: function () { return []; }
+};
+
+module.exports = BaseCartHelpers;
